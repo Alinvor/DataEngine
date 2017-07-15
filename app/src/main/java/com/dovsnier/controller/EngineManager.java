b1e40f19802e4ae64ea91434a972edf223289efb
DovSnier <1305314861@qq.com>
2017-07-15 14:21:48 +0800

    EngineManager optimization

--- app/src/main/java/com/dovsnier/controller/EngineManager.java
+++ app/src/main/java/com/dovsnier/controller/EngineManager.java
@@ -1,44 +1,25 @@
 package com.dovsnier.controller;
 
 import com.dvsnier.controller.Manager;
-import com.dvsnier.widget.LifeCycle;
-
-import org.xutils.DbManager;
 
 /**
  * Created by lizw on 2017/7/14.
  */
 
-public class EngineManager implements LifeCycle {
+public class EngineManager extends Manager {
 
     private static final String TAG = EngineManager.class.getSimpleName();
     private static final EngineManager instance = new EngineManager();
 
-    private Manager manager;
-
-    private EngineManager() {
-        manager = Manager.getInstance();
+    protected EngineManager() {
     }
 
     public static EngineManager getInstance() {
         return instance;
     }
 
-    public Manager getManager() {
-        return manager;
-    }
-
-    public void setManager(Manager manager) {
-        this.manager = manager;
-    }
-
-    public DbManager getDbManager() {
-        return Manager.getInstance().getDbManager();
-    }
-
     @Override
     public void onDestroy() {
-        if (null != manager)
-            manager.onDestroy();
+        super.onDestroy();
     }
 }
