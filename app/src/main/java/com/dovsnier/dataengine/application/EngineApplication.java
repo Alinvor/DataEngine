0d17c5f99abd3d8b32bfae6363cd24b1dd33c2b4
lizw <lizw@ichsy.com>
2017-07-17 17:04:26 +0800

    compatible with the leakcanary version under api 14

--- app/src/main/java/com/dovsnier/dataengine/application/EngineApplication.java
+++ app/src/main/java/com/dovsnier/dataengine/application/EngineApplication.java
@@ -2,6 +2,8 @@ package com.dovsnier.dataengine.application;
 
 import com.dovsnier.controller.EngineManager;
 import com.dvsnier.application.BaseApplication;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 /**
  * Created by lizw on 2017/7/14.
@@ -9,15 +11,32 @@ import com.dvsnier.application.BaseApplication;
 
 public class EngineApplication extends BaseApplication {
 
+    protected static EngineApplication instance;
+    protected RefWatcher refWatcher;
+
+    public static EngineApplication getInstance() {
+        return instance;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
+        instance = this;
         EngineManager.getInstance().initialized(this);
-//        if (LeakCanary.isInAnalyzerProcess(this)) {
-//            // This process is dedicated to LeakCanary for heap analysis.
-//            // You should not init your app in this process.
-//            return;
-//        }
-//        LeakCanary.install(this);
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        refWatcher = LeakCanary.install(this);
+    }
+
+
+    public RefWatcher getRefWatcher() {
+        return refWatcher;
+    }
+
+    public void setRefWatcher(RefWatcher refWatcher) {
+        this.refWatcher = refWatcher;
     }
 }
